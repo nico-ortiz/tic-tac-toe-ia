f7b917b3922beddf49f77d2feb3fe20cac38a8d4
Nicolas-Ortiz <nicortiz1234@gmail.com>
2023-09-28 16:36:48 -0300

    Create Minimax algorithm with Alpha-Beta pruning

---
+++ src/main/java/com/project/engine/Minimax.java
@@ -0,0 +1,93 @@
+package com.project.engine;
+
+import java.util.List;
+
+import com.project.aditionalFunc.Tuple;
+import com.project.search.TicTacToeState;
+import com.project.search.TicTacToeStateProblem;
+
+public class Minimax {
+    
+    private TicTacToeStateProblem p;
+    private int maxDepth;
+
+    public Minimax(TicTacToeStateProblem p, int maxDepth) {
+        this.p = p;
+        if (maxDepth == 0) throw new IllegalArgumentException("maximum depth must be more biggest than 1");
+        this.maxDepth = maxDepth;
+    }
+    
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    public void setMaxDepth(int maxDepth) {
+        if (maxDepth <= 0) throw new IllegalArgumentException("maximum depth must be more biggest or equal than 1");
+        this.maxDepth = maxDepth;
+    }
+
+    public TicTacToeStateProblem getProblem() {
+        return p;
+    }
+
+    public void setProblem(TicTacToeStateProblem p) {
+        if (p == null) throw new IllegalArgumentException("invalid value");
+        this.p = p;
+    }
+
+    //Hacer el minimax fuera de este metodo
+    public int computeValue(TicTacToeState state) {
+        if (state == null) 
+            throw new IllegalArgumentException("invalid state");
+        return miniMax(state, this.maxDepth, Integer.MIN_VALUE, Integer.MAX_VALUE).getSnd();
+    }
+
+    public TicTacToeState computeSuccessor(TicTacToeState state) {
+        if (state == null)
+            throw new IllegalArgumentException("invalid state");
+        return miniMax(state, this.maxDepth, Integer.MIN_VALUE, Integer.MAX_VALUE).getFst();
+    }
+
+    /**
+     * 
+     * @param state
+     * @return Return a tuple, first component is the most prommising succesor for state.
+     * The second component is the value of the most prommisin succesor.
+     */
+    private Tuple<TicTacToeState> miniMax(TicTacToeState state, int maxDepth, int alpha, int beta) {
+        if (state.end() || maxDepth == 0) {
+            return new Tuple<TicTacToeState>(state, state.value());
+        } else {
+            int minTemp = Integer.MAX_VALUE;
+            int maxTemp = Integer.MIN_VALUE;
+            TicTacToeState maxState = null, minState = null;
+
+            maxDepth--;
+            if (alpha < beta) {
+                List<TicTacToeState> succs = p.getSuccessors(state);
+                for (TicTacToeState succ: succs) {
+                    if (state.isMax()) {
+                        alpha = Math.max(alpha, miniMax(succ, maxDepth, alpha, beta).getSnd());
+                        if (maxTemp < alpha) {
+                            maxState = succ;
+                            maxTemp  = alpha;
+                        }
+                    } else {
+                        beta = Math.min(beta, miniMax(succ, maxDepth, alpha, beta).getSnd());
+                        if (minTemp > beta) {
+                            minState = succ;
+                            minTemp  = beta;
+                        }
+                    }
+                }
+            }   
+
+            if (state.isMax()) {
+                return new Tuple<TicTacToeState>(maxState, alpha);
+            } else {
+                return new Tuple<TicTacToeState>(minState, beta);
+            }
+        }
+    }
+
+}
